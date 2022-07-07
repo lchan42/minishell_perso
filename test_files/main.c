a8355caeeee2f01076c9858ba6ab86446c9e5e2f
Luc CHAN <lchan@e1r5p19.42.fr>
2022-07-07 17:40:38 +0200

    need to add history correctly

--- test_files/main.c
+++ test_files/main.c
@@ -1,19 +1,27 @@
 #include "../minishell.h"
 
+// a function to clear the wrong history list
+
 int main ()
 {
-	char *read;
-	read = NULL;
+	char	*read_full;
+	char	*prompt;
+	t_list	*history_list;
 
-	while (!read)
+	prompt = NULL;
+	history_list = (t_list *)0;
+	read_full = ft_strdup("");
+	while (1)
 	{
-		read = readline("minishell: ");
-		if (read)
-			while (!*read)
-			{
-				free(read);
-				read = readline("minishell: ");
-			}
+		prompt = readline("minishell: ");
+		if (*(prompt + strlen(prompt) - 1) == '|')
+		{
+			rl_replace_line(read_full, 1);
+			read_full = ft_strjoin(read_full , prompt);
+			printf("Value of read_full %s\n", read_full);
+		}
+		add_history(read_full);
+		//free(read);
 	}
-	free(read);
+	//free(read);
 }
