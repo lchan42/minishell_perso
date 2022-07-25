519fcdd9258576c9a30620e0918f0491f9f97ad9
Sacha LAHLOU <slahlou@e1r1p5.clusters.42paris.fr>
2022-07-25 18:52:30 +0200

    mal de crane de lexpand

--- expand.c
+++ expand.c
@@ -6,7 +6,7 @@
 /*   By: slahlou <slahlou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 11:12:12 by slahlou           #+#    #+#             */
-/*   Updated: 2022/07/25 17:09:13 by slahlou          ###   ########.fr       */
+/*   Updated: 2022/07/25 18:47:38 by slahlou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,32 @@ char	*__is_expandable(char *str)
 	static char	quote_flag1 = 0;
 
 	if (*str == '\0')
+	{
+		quote_flag1 = 0;
 		return (NULL);
+	}
 	if (quote_flag1 == 0 || quote_flag1 == '\'')
 		quote_flag1 ^= (*str == '\'') * '\'';
 	if (quote_flag1 == 0 || quote_flag1 == '\"')
 		quote_flag1 ^= (*str == '\"') * '\"';
 	if (*str == '$'
-		&& (ft_isalnum((*(str + 1))) ||  ft_strchr_b("_\"\'", *(str + 1)))//*(str + 1) == '_' || *(str + 1) == '\"' || *(str + 1) == '\'')
+		&& (ft_isalnum((*(str + 1))) ||  ft_strchr_b("_\"\'", *(str + 1)))
 		&& *(str + 1) && quote_flag1 != '\'')
-		if (!(*(str + 1) == quote_flag1))
-			return (str);
+		{
+			if (!(*(str + 1) == quote_flag1))
+			{
+				if (!(quote_flag1 == '\"' && *(str + 1) == '\''))
+					return (str);
+			}
+		}
 	return (__is_expandable(str + 1));
 }
 
-//test'$USER' $"USER" $'USER' "$USER" $PATH "'$USER'"
+//test'$USER' $"USER" $'USER' "$USER" $PATH "'$USER'" "$'USER'" $$$USER $"$'USER"
 // erreur sur :
 // $'USER'
 // "'$USER'"
+// "$'USER'"
 
 char	*__get_expand(char *start, int len, char **env)
 {
@@ -46,22 +55,23 @@ char	*__get_expand(char *start, int len, char **env)
 	return (*env);
 }
 
-char	*__add_exp_to_str(char *str, char *start, char *end, char *expand)
+char	*__add_exp_to_str(char *str, char **start, char *end, char *expand)
 {
 	int	len_str;
 	int	full_len;
 	char	*tmp;
 
 	len_str = ft_strlen_p(str);
-	full_len = (len_str - 1) - (end - start) + ft_strlen_p(expand);
+	full_len = (len_str - 1) - (end - *start) + ft_strlen_p(expand);
 	tmp = ft_calloc(sizeof(char), full_len + 1);
 	if (!tmp)
 		return (NULL);
-	while(str != start - 1)
+	while(str != *start - 1)
 		*(tmp++) = *(str++);
 	str = end;
 	while (expand && *expand)
 		*(tmp++) = *(expand++);
+	*start = tmp;
 	while (*str)
 		*(tmp++) = *(str++);
 	free (str - len_str);
@@ -80,13 +90,15 @@ char	*__expand_string(char *str, char **env)
 		start = __is_expandable(start);
 		if (start)
 		{
+			printf("%s is expandable\n", start);
 			end = ++start;
-			while (*end && (ft_isalnum(*end) || *end == '_'))
-				end++;
+			if (!ft_isdigit(*end))
+				while (*end && (ft_isalnum(*end) || *end == '_'))
+					end++;
 			expand = __get_expand(start, end - start, env);
 			//printf("expand = %s\n", expand);
-			start = __add_exp_to_str(str, start, end, expand);
-			str = start;
+			str = __add_exp_to_str(str, &start, end, expand);
+			//str = start;
 		}
 	}
 	return (str);
